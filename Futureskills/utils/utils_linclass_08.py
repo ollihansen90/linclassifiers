d7f0ab32dba282f1d58b968a6e448cf8456d304b
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2025-04-01 12:58:54 +0200

    Buffer angepasst, da Matplotlib 3.10 anders Bilder erstellt oder so

--- Futureskills/utils/utils_linclass_08.py
+++ Futureskills/utils/utils_linclass_08.py
@@ -23,7 +23,7 @@ def draw(w, data, label):
     plt.scatter(data[label==-1,0], data[label==-1,1])
     plt.axis("scaled");plt.xlim([-size,size]);plt.ylim([-size,size]);plt.axis("off")
     fig.canvas.draw()
-    image_flat = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
-    image = image_flat.reshape(*reversed(fig.canvas.get_width_height()), 3)
+    image_flat = np.frombuffer(fig.canvas.buffer_rgba(), dtype='uint8')
+    image = image_flat.reshape(*reversed(fig.canvas.get_width_height()), 4)[...,:3]
     plt.close()
     return image
